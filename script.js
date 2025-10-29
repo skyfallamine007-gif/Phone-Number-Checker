8bbb36ee9fd4fa43a61ae99b8535f546484f4228
Amine <skyfallamine007@gmail.com>
2025-10-30 00:22:46 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+const checkBotton = document.getElementById("check-btn");
+const clearBotton = document.getElementById("clear-btn");
+const resultsDiv = document.getElementById("results-div");
+const userInput = document.getElementById("user-input");
+checkBotton.addEventListener("click" , handleCheck);
+clearBotton.addEventListener("click" , handleClear);
+
+function handleCheck() {
+  const check = userInput.value.trim();
+
+  if (check === "") {
+    alert("Please provide a phone number");
+    return;
+  }
+
+  const regex = /^(1\s?)?(\(\d{3}\)|\d{3})([\s\-])?\d{3}([\s\-])?\d{4}$/;
+
+  if (regex.test(check)) {
+    resultsDiv.textContent = "Valid US number: " + check;
+  } else {
+    resultsDiv.textContent = "Invalid US number: " + check;
+  }
+}
+
+
+
+function handleClear() {
+      userInput.value = "";
+      resultsDiv.textContent = "";
+}
